b856614f253a4fcf1fcc428b3d690600d5db5a29
robshan <robshanahan5@proton.me>
2025-12-26 17:29:33 +0000

    adding hardware config for server

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -4,6 +4,7 @@
     networking.hostName = "server"; # Define your hostname.
 
     imports = [
+        ./hardware-configuration.nix
         ../../configuration.nix
         ../../system/users/nix_server.nix
         ../../system/jellyfin/default.nix

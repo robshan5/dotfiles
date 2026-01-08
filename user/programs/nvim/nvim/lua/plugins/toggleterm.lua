03fe620d8631abf50bef53703d49b58e512b6d6e
robshan5 <robshanahan5@proton.me>
2026-01-08 16:33:47 +0000

    updating toggleterm

--- user/programs/nvim/nvim/lua/plugins/toggleterm.lua
+++ user/programs/nvim/nvim/lua/plugins/toggleterm.lua
@@ -1,28 +1,29 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  opts = { --[[ things you want to change go here]]
-  },
-  config = function()
-    require("toggleterm").setup({
-      size = 20,
-      open_mapping = [[<c-/>]],
-      direction = "float",
-      start_in_insert = true,
-      close_on_exit = true,
-      persist_size = true,
-      insert_mappings = true,
-      hide_numbers = true,
-      shade_filetypes = {},
-      shading_factor = 2,
-      shell = vim.o.shell,
-      float_opts = {
-        winblend = 0,
-        highlights = {
-          border = "Normal",
-          background = "Normal",
-        },
-      },
-    })
-  end,
+	"akinsho/toggleterm.nvim",
+	version = "*",
+	opts = { --[[ things you want to change go here]]
+	},
+	config = function()
+		require("toggleterm").setup({
+			size = vim.o.columns * 0.4,
+			persist_size = false,
+			open_mapping = [[<c-/>]],
+			direction = "floating",
+			start_in_insert = true,
+			close_on_exit = true,
+			persist_size = true,
+			insert_mappings = true,
+			hide_numbers = true,
+			shade_filetypes = {},
+			shading_factor = 2,
+			shell = "zsh",
+			float_opts = {
+				winblend = 0,
+				highlights = {
+					border = "Normal",
+					background = "Normal",
+				},
+			},
+		})
+	end,
 }

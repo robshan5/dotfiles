ced352683ae7f536e506ed631872f8b2e50c0195
robshan5 <robshanahan5@proton.me>
2026-04-02 16:50:34 +0100

    adding tailscale client to laptop

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -3,16 +3,12 @@
 {
     networking.hostName = "laptop"; # Define your hostname.
 
-  #   networking.extraHosts = ''
-  # 192.168.15.217 nextcloud.local
-  # 192.168.15.217 collabora.local
-  #   '';
-
     imports = [
         ./hardware-configuration.nix
         ../../configuration.nix
         ../../system/users/robshan.nix
         ../../system/wm/default.nix
         ../../system/audio/default.nix
+        ../../system/tailscale/client.nix
     ];
 }

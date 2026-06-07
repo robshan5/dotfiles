3962907dabc8e58a4587f36e24b847d8f44bf118
robshan <robshanahan5@proton.me>
2026-06-07 12:35:40 +0100

    pushing server stuff

--- system/nextcloud/default.nix
+++ system/nextcloud/default.nix
@@ -16,11 +16,24 @@
         settings = {
             trusted_domains = [
                 "192.168.15.217"
+                "100.106.29.22"
+                "cloud.robshan.space"
                 "localhost"
             ];
         };
     };
 
     services.nginx.enable = true;
+    # services.nginx.virtualHosts."cloud.robshan.space" = {
+    #     locations."/" = {
+    #         proxyPass = "http://127.0.0.1:8080";
+    #     #     proxyPreserveHost = true;
+    #     };
+    #     enableACME = true;
+    # };
+
+    # security.acme.certs."cloud.robshan.space".email = "robshanahan@duck.com";
+    # security.acme.acceptTerms = true;
+
     networking.firewall.allowedTCPPorts = [ 80 442 ];
 }
